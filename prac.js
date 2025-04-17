65d02a2509384060299b4ce2cad690dd3b6da043
Welcome <welcome@saurabh-yaduvanshi.local>
2025-04-17 23:45:19 +0530

    Initial commit

---
+++ prac.js
@@ -0,0 +1,27 @@
+// console.log(a);
+// var a = 10;
+// console.log(b);
+// let b = 20;
+
+// this.name = "Shivraj";
+// const obj = {
+//   name: "John",
+//   getName: function () {
+//     return this.name;
+//   },
+//   getArrowName: () => {
+//     return this.name;
+//   },
+// };
+
+const obj = {
+  name: "Alice",
+  printName: function () {
+    //   printName() {
+    setTimeout(function () {
+      console.log(this.name);
+    }, 1000);
+  },
+};
+
+obj.printName();
